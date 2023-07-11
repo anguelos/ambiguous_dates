55ae894722d2782bcbe6f80cc57c5d6152f9c3f0
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2023-07-11 03:46:23 +0200

    posished up

--- tkinter_probabilistic_date/probable_date_widget.py
+++ tkinter_probabilistic_date/probable_date_widget.py
@@ -20,58 +20,35 @@ import sys
 t_pixel_value = Union[int, float]
 
 
-class DebouncedDoubleVar(DoubleVar):
-    def __init__(self, master=None, value=None, delay=500):
-        super().__init__(master, value)
-        self.trace_id = None
-        self.delay = delay
-
-    def debounce_callback(self, *args):
-        if self.trace_id is not None:
-            self.master.after_cancel(self.trace_id)
-        self.trace_id = self.master.after(self.delay, self.update_callback)
-
-    def update_callback(self):
-        self.trace_id = None
-        # Perform your desired action here
-
-    def trace(self, mode, callback):
-        if mode == 'write':
-            super().trace(mode, self.debounce_callback)
-        else:
-            super().trace(mode, callback)
-
-
 class DateRangeSlider(Frame):
     def show_small_form(self):
-        print('show_small_form')
         if self.large_form:
             self.large_form = False
-            # self.update_btn.pack_forget()
             self.update_btn.config(text='Edit')
             self.curve_frame.pack_forget()
-            self.canvas.get_tk_widget().pack_forget()
             self.rs1.pack_forget()
+            self.mode_frame.pack_forget()
+            self.comment_frame.pack_forget()
             self.txtbox_from.config(state='readonly')
             self.txtbox_to.config(state='readonly')
             self.update()
 
     def show_large_form(self):
-        print('show_large_form')
         if not self.large_form:
             self.large_form = True
-            # self.update_btn.pack()
             self.update_btn.config(text='Set')
-            self.curve_frame.pack()
-            self.canvas.get_tk_widget().pack()
+            self.curve_frame.pack(side="top", anchor="w",
+                                  fill='x', expand=True)
             self.rs1.pack()
+
+            self.mode_frame.pack(anchor="w", fill='x', expand=True)
+            self.comment_frame.pack(anchor="w", fill='x', expand=True)
+
             self.txtbox_from.config(state='normal')
             self.txtbox_to.config(state='normal')
             self.update()
 
     def toggle_form(self):
-        print(
-            f"Toggle between large and small forms Large_form=={self.large_form}")
         if self.large_form:
             self.show_small_form()
         else:
@@ -83,7 +60,6 @@ class DateRangeSlider(Frame):
         else:
             self.counter += 1
 
-        print(f"{self.counter} update_curve")
         self.ax.clear()
         self.ax.set_xlim(self.earliest_date[0], self.latest_date[0]+1)
 
@@ -99,9 +75,33 @@ class DateRangeSlider(Frame):
             gaussian = gaussian / np.sum(gaussian)
 
         self.ax.set_ylim(0, 1.5*max(np.max(uniform), np.max(gaussian)))
-        self.ax.plot(self.years, uniform)
-        self.ax.plot(self.years, gaussian)
+        if self.mode_var.get() == "Uniform":
+            unif_strokewidth = 3
+            gauss_strokewidth = 1
+        elif self.mode_var.get() == "Gaussian":
+            unif_strokewidth = 1
+            gauss_strokewidth = 3
+        else:
+            raise ValueError(
+                f"Unknown mode_var value: {self.mode_var.get()}")
+        self.ax.plot(self.years, uniform, linewidth=unif_strokewidth)
+        self.ax.plot(self.years, gaussian, linewidth=gauss_strokewidth)
         self.canvas.draw()
+
+    def get_comment(self):
+        return self.comment_text.get("1.0", "end-1c")
+
+    def get_to(self):
+        return self.from_Var.get()
+
+    def get_from(self):
+        return self.from_Var.get()
+
+    def get_around(self):
+        return (self.to_Var.get() + self.from_Var.get())/2
+
+    def get_giveortake(self):
+        return (self.to_Var.get() - self.from_Var.get())/2
 
     def __init__(self, earliest_date: Tuple[int, int, int] = (0, 0, 0),
                  latest_date: Tuple[int, int, int] = (2023, 0, 0),
@@ -118,9 +118,9 @@ class DateRangeSlider(Frame):
                  highlightbackground: str = ..., highlightcolor: str = ...,
                  highlightthickness: t_pixel_value = ..., name: str = ..., padx: t_pixel_value = ...,
                  pady: t_pixel_value = ..., relief: Any = ..., takefocus: Any = ...,
-                 visual: Union[str, Tuple[str, int]] = ..., width: t_pixel_value = ...
+                 visual: Union[str, Tuple[str, int]] = ..., width: t_pixel_value = 500
                  ) -> None:
-        super().__init__(master,
+        super().__init__(master, highlightthickness=1, highlightbackground="darkgrey"
                          # cnf,
                          # background=background, bd=bd, bg=bg, border=border,
                          # borderwidth=borderwidth, class_=class_, colormap=colormap,
@@ -132,50 +132,63 @@ class DateRangeSlider(Frame):
         self.earliest_date = earliest_date
         self.latest_date = latest_date
         self.years = np.arange(earliest_date[0], latest_date[0]+1)
-        self.fig = Figure(figsize=(5, 1), dpi=100)
+        self.fig = Figure(figsize=(((width)/100), 1), dpi=100)
         self.ax = self.fig.add_subplot(111)
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
         self.curve_frame = Frame(self)
         self.curve_frame.pack()
+
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.curve_frame)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack()
+        self.canvas.get_tk_widget().pack(side="left", anchor="w")
+
+        self.mode_var = tk.StringVar()
+        self.mode_var.set("Uniform")
+
         self.from_Var = DoubleVar()  # left handle variable
-        # self.from_Var.trace_add('write', self.update_curve)
         self.from_Var.trace_add('write', self.update_curve)
         self.to_Var = DoubleVar()  # delay=100)  # left handle variable
-        # self.to_Var.trace_add('write', self.update_curve)
-        self.rs1 = RangeSliderH(self, [self.from_Var, self.to_Var], Width=500, Height=65, padX=60, min_val=self.earliest_date[0],
+        self.to_Var.trace_add('write', self.update_curve)
+
+        self.rs1 = RangeSliderH(self, [self.from_Var, self.to_Var], Width=width, Height=65, padX=60, min_val=self.earliest_date[0],
                                 max_val=self.latest_date[0]+1, show_value=True)
         self.rs1.pack()
-        self.from_to_frame = Frame(self)
-        self.from_to_frame.pack()
 
-        Label(self.from_to_frame, text="From:").pack(side="left")
+        self.comment_frame = tk.Frame(self)
+        self.comment_frame.pack()
+        self.comment_label = tk.Label(self.comment_frame, text="Comment:")
+        self.comment_label.pack(side="top", anchor="w")
+        self.comment_text = tk.Text(self.comment_frame, height=5, width=50)
+        self.comment_text.pack(side="top", anchor="w", fill='x', expand=True)
+
+        self.from_to_frame = Frame(self)
+        self.from_to_frame.pack(anchor="w", fill='x', expand=True)
+        self.update_btn = Button(
+            self.from_to_frame, text="Set", command=self.toggle_form)
+        self.update_btn.pack(side="left")
+        Label(self.from_to_frame, text="From Year:").pack(side="left")
         self.txtbox_from = Entry(
             self.from_to_frame, textvariable=self.from_Var, width=10)
         self.txtbox_from.pack(side="left")
         self.txtbox_from.bind("<KeyRelease>", self.update_curve)
 
-        Label(self.from_to_frame, text="To:").pack(side="left")
+        Label(self.from_to_frame, text="To Year:").pack(side="left")
         self.txtbox_to = Entry(
             self.from_to_frame, textvariable=self.to_Var, width=10)
+        self.txtbox_to.bind("<KeyRelease>", self.update_curve)
 
         self.txtbox_to.pack(side="left")
-        self.update_btn = Button(
-            self.from_to_frame, text="Set", command=self.toggle_form)
-        self.txtbox_to.bind("<KeyRelease>", self.update_curve)
-        self.update_btn.pack(side="right")
+
+        self.mode_frame = tk.Frame(self)
+        self.mode_frame.pack(anchor="w", fill='x', expand=True)
+
+        self.mode_uniform_button = tk.Radiobutton(
+            self.mode_frame, text="From - To", variable=self.mode_var, value="Uniform", command=self.update_curve)
+        self.mode_uniform_button.pack(side="left", anchor="w")
+
+        self.mode_gaussian_button = tk.Radiobutton(
+            self.mode_frame, text="Give or Take", variable=self.mode_var, value="Gaussian", command=self.update_curve)
+        self.mode_gaussian_button.pack(side="left", anchor="w")
+
         self.large_form = True
         self.show_small_form()
         self.update_curve()
-
-
-if __name__ == "__main__":
-    root = Tk()
-    root.geometry("750x750")
-    root.title("Date Range Slider")
-    drs = DateRangeSlider(master=root)
-    drs.pack()
-    root.mainloop()
-    sys.exit(0)
